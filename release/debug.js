e62d920aad594da3ef0bbfe9b4261726b1d3fc66
Matthias Buchholz <matthias@afkm.io>
2017-07-06 02:26:58 +0200

    debugging fancy

--- release/debug.js
+++ release/debug.js
@@ -3,7 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const prefix = 'rxfs';
 exports.log = (format, ...args) => {
     if (process.env.NODE_ENV && process.env.NODE_ENV === 'debug') {
-        console.log(`%s ${format}`, prefix, ...args);
+        console.log(`\x1b[33m[%s]\x1b[0m ${format}`, prefix, ...args);
     }
 };
 //# sourceMappingURL=debug.js.map
